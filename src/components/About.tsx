d61d1ccaef4e98eff29ce03fc0d615568842fe58
Scott <skenning128@gmail.com>
2023-10-17 14:37:31 -0700

    fix typo

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -7,7 +7,7 @@ export default function About() {
         <div className="w-screen flex flex-col items-center justify-center text-base lg:text-sm 2xl:text-xl text-left p-8 max-w-2xl 2xl:max-w-4xl m-auto">
             <Image src={heroImage} alt="Hero Image" width={100} height={100} className="w-36 2xl:w-48 mb-4 rounded-full shadow shadow-white"/>
             <p>
-            Hello! I&apos;m Scott Kening, a full-stack developer and software engineering student at the University of Victoria. I specialize in Typescript and Javascript, but I enjoy writing code and solving problems in any language. Beyond the usual coursework, I&apos;m leading a student project called StudySpaceFinder, a website that helps find empty classrooms on campus. When I&apos;m not staring at a screen, I enjoy going on runs and the occasional game of golf.</p>
+            Hello! I&apos;m Scott Kenning, a full-stack developer and software engineering student at the University of Victoria. I specialize in Typescript and Javascript, but I enjoy writing code and solving problems in any language. Beyond the usual coursework, I&apos;m leading a student project called StudySpaceFinder, a website that helps find empty classrooms on campus. When I&apos;m not staring at a screen, I enjoy going on runs and the occasional game of golf.</p>
             <div className="grid md:grid-cols-2 gap-8 mt-4">
                 <div className="flex flex-col gap-2">
                 <p className="font-mono border-b border-gray-400">Languages</p>
